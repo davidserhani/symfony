0c4c1a3b12003ca528f1576bb5277ef9256ce7a1
David Serhani <davidserhani@gmail.com>
2018-06-27 17:03:49 +0200

    controller & route added

---
+++ BeerTime/src/Controller/MainController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Etudiant
+ * Date: 27/06/2018
+ * Time: 16:59
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class MainController
+{
+    public function homepage() {
+        return new Response();
+    }
+}
